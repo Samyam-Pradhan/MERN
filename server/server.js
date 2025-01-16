be6aa18514d9d4903fe62d18b794c459c27377e5
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-01-16 20:20:38 +0545

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.status(200).send('Welcome to mern ');
+});
+
+const PORT = 5000;
+app.listen(PORT, ()=>{
+    console.log(`server running at port ${PORT}`);
+    
+})
