85814641d1103f1eb75a0a15b4294eb13a7ade69
Samyam-Pradhan <pradhansamyam71@gmail.com>
2025-01-17 20:25:19 +0545

    Connect to Db

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const router = require('./router/auth-router');
+const connectDb = require("./utils/db");
 
 app.use(express.json());
 
 app.use("/api/auth", router);
 
 const PORT = 5000;
+
+connectDb().then(()=> {
 app.listen(PORT, ()=>{
     console.log(`server running at port ${PORT}`);
     
 })
+});
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,4 +1,18 @@
 const mongoose = require("mongoose");
 
 const URI =  "mongodb://127.0.0.1:27017/mern_admin";
-mongoose.connect(URI);
+/* mongoose.connect(URI); */
+
+const connectDb = async () =>{
+    try {
+        await mongoose.connect(URI);
+        console.log("Connection successful to DB");
+        
+    } catch (error) {
+        console.log("Database connection failed");
+        process.exit(0);
+        
+    }
+}
+
+module.exports = connectDb;
